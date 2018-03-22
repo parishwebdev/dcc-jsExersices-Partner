e59d5a1d74f332be168854d5584275ad191cd6cf
parishwebdev <parishwebdev@gmail.com>
2018-03-21 20:59:04 -0500

    completed #5

--- app.js
+++ app.js
@@ -2,6 +2,10 @@
 function displayResult (param){
 	console.log(param);
 }
+function getUserInput(message) {
+  let response = prompt(message);
+  return response;
+}
 
 // #1
 /*
@@ -59,6 +63,7 @@ displayResult(captilzeFirstLetter(makeLowCaseOnSpace("hello i am learning.")));
 
 // #4
 //left off here
+/*
 function CheckForRepeat(originalString) {
     let repeatCount = 1;
     let holderLet = "";
@@ -75,14 +80,61 @@ function CheckForRepeat(originalString) {
         		holderLet += repeatCount + originalString.charAt(i-1)
         		repeatCount = 1;
         	}
-        else {
-        	holderLet += repeatCount + originalString.charAt(i-1);
-
-        }   
     }
     return holderLet;
 }
-displayResult(CheckForRepeat("aaabbbbcc"));
+displayResult(CheckForRepeat("aaabbbblll"));*/
+
+//#5
+
+function forwardStringToArray(ftxt){
+let fowArray = [];
+
+	for (let f = 0; f < ftxt.length; f++){
+		fowArray.push(ftxt[f]); 
+	}
+
+	return fowArray;
+}
+forwardStringToArray("madam");
+
+function backwardsStringToArray(btxt){
+let bacArray = [];
+
+	for (let b = btxt.length-1 ; b >= 0; b--){
+		bacArray.push(btxt[b]);
+	}
+
+	return bacArray;
+}
+backwardsStringToArray("madam");
+
+
+function compareTwoArrayValues(array1,array2){
+let yesCount = 0;
+let notCount = 0;
+for (let i = 0; i < array1.length; i++) {
+	if(array1[i] == array2[i]){
+		yesCount++;
+		if (yesCount == array1.length){
+		return "Yes Palindrome";
+	    }
+	}else if (array1[i] != array2[i]){
+		return "Not a Palindrome";
+		break;
+	}
+}
+
+}
+function runExersice5() {
+	let input = getUserInput("Enter a word to see if it is a Palindrome");
+	displayResult(compareTwoArrayValues(forwardStringToArray(input),backwardsStringToArray(input)));
+}
+runExersice5();
+
+
+
+
 
 
 
